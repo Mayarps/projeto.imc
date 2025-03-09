e06ab0a66f9c02ae524457ad498ff34f79023293
Mayarps <mayara-pinheiro@hotmail.com>
2025-03-09 17:35:38 -0300

    Add files via upload

---
+++ imc.py
@@ -0,0 +1,18 @@
+
+def calcular_imc(peso, altura):
+    return peso / (altura * altura)  
+
+
+def classificar_imc(imc):
+    if imc < 18.5:
+        return "Abaixo do peso"
+    elif imc < 24.9:
+        return "Peso normal"
+    elif imc < 29.9:
+        return "Sobrepeso"
+    return "Obesidade"
+
+peso = 70
+altura = 1.75
+imc = calcular_imc(peso, altura)
+print(f"IMC: {imc:.2f}, Classificação: {classificar_imc(imc)}")
